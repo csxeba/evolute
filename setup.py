a3c9fef4675e8b118f02cba8085791ee47e8248c
csxeba <csxeba@gmail.com>
2018-08-15 22:00:17 +0200

    Typo

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.9.0',
     packages=find_packages(),
     url='https://github.com/csxeba/evolute.git',
-    license='GPLv3',
+    license='MIT',
     author='Csaba Gór',
     author_email='csxeba@gmail.com',
-    description='Evolutionary algoritm toolbox'
+    description='Evolutionary algorithm toolbox'
 )
